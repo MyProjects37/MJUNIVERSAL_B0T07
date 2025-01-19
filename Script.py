8fab9f350618cb7da07089c5210a098850c22e1a
MyProjects37 <hiih0286@gmail.com>
2025-01-19 06:59:16 -0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -251,14 +251,14 @@ Nᴀᴍᴇ - {}"""
 IMDB_TEMPLATE_TEXT = "path/to/imdb_template.html"
 Query: {qurey}
 
-<b>Title</b>: <a href="{url}">{title}</a>
- Genres: {genres}
- Year: <a href={url}/releaseinfo>{year}</a>
- Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
- Languages : <code>{languages}</code>
- RunTime: {runtime} Minutes
- Release Info : {release_date}
- Countries : <code>{countries}</code> 
+<b><a href="{url}">{title}</a></b>
+Genres: {genres}
+Year: <a href={url}/releaseinfo>{year}</a>
+Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
+Languages : <code>{languages}</code>
+RunTime: {runtime} Minutes
+Release Info : {release_date}
+Countries : <code>{countries}</code> 
 
 
 # Result Shown in: {remaining_seconds} <i>seconds</i> 
