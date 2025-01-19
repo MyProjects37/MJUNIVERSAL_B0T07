d1d693f3427720f6a83ce1a05e32cc503b03384f
MyProjects37 <hiih0286@gmail.com>
2025-01-19 08:13:43 -0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -248,22 +248,24 @@ Nᴀᴍᴇ - {}"""
 
 """<b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
 
-IMDB_TEMPLATE = "path/to/imdb_template.html"
-# Query: {qurey}
+IMDB_TEMPLATE_TXT = """
+<b>Query: {qurey}
 
-# Title: <a href={url}>{title}</a>
-# Genres: {genres}
-# Year: <a href={url}/releaseinfo>{year}</a>
-# Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-# Languages : <code>{languages}</code>
-# RunTime: {runtime} Minutes
-# Release Info : {release_date}
-# Countries : <code>{countries}</code> 
+IMDb Data:
+
+<b>Title</b>: <a href={url}>{title}</a>
+Genres: {genres}
+Year: <a href={url}/releaseinfo>{year}</a>
+Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
+Languages : <code>{languages}</code>
+RunTime: {runtime} Minutes
+Release Info : {release_date}
+Countries : <code>{countries}</code>
 
 
-# Result Shown in: {remaining_seconds} <i>seconds</i> 
+Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
 
-"""<b> Requested_by: {message.from_user.mention}</b>"""
+Requested by : {message.from_user.mention}</b>"""
     
     # ALL_FILTERS = """
 # """<b>Hᴇʏ {}, Tʜᴇsᴇ ᴀʀᴇ ᴍʏ ᴛʜʀᴇᴇ ᴛʏᴘᴇs ᴏғ ғɪʟᴛᴇʀs.</b>"""
