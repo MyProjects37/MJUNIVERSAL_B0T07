b25094dddf80d32123b6ab6abd947b753f03f54d
MyProjects37 <hiih0286@gmail.com>
2025-01-19 07:06:13 -0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -251,7 +251,7 @@ Nᴀᴍᴇ - {}"""
 IMDB_TEMPLATE_TEXT = "path/to/imdb_template.html"
 Query: {qurey}
 
-Title</b>: <a href={url}>{title}</a>
+Title: <a href={url}>{title}</a>
 Genres: {genres}
 Year: <a href={url}/releaseinfo>{year}</a>
 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
