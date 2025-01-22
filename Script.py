733ee82e15b33b8b6a745f2ef6542063b9ccf82b
MyProjects37 <hiih0286@gmail.com>
2025-01-21 21:43:44 -0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -248,20 +248,20 @@ Nᴀᴍᴇ - {}"""
 
 """<b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
 
-IMDB_TEMPLATE_TXT = "IMDB_TEMPLATE"
-Query: {qurey}
+IBDB_TEMPLATE_TEXT = "IBDB_TEMPLATE_TEXT"
+query: "{qurey}"
 
-IMDbData:
+IMDbData: "ImdbData"
 
-<b>Title</b>: <a href={url}>{title}</a>
-Genres: {genres}
-Year: <a href={url}/releaseinfo>{year}</a>
-Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-Languages : <code>{languages}</code>
-RunTime: {runtime} Minutes
-Release Info : {release_date}
-Countries : <code>{countries}</code>
-
+Title: "<a href={url}>{title}</a>"
+Genres: "{genres}"
+Year: "<a href={url}/releaseinfo>{year}</a>"
+Rating: "<a href={url}/ratings>{rating}</a> / 10;"
+('based on {votes} user ratings.')
+Languages : '<code>{languages}</code>'
+RunTime: '{runtime} Minutes'
+ReleaseInfo : {'release_date'}
+Countries : '<code>{countries}</code>'
 
 Result Shown in: {remaining_seconds} <i>seconds</i>
 
