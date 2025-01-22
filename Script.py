ce9537ced508e5503c3425bff0779a5857aa6212
MyProjects37 <hiih0286@gmail.com>
2025-01-21 22:45:27 -0800

    Update Script.py

--- Script.py
+++ Script.py
@@ -248,10 +248,10 @@ Nᴀᴍᴇ - {}"""
 
 """<b>⚙️ sɪᴢᴇ : {file_size}</b>""" 
 
-IMDB_TEMPLATE_TEXT = "IMDB_TEMPLATE_TXT"
+IMDB_TEMPLATE_TXT = """
 query: "{qurey}"
 
-IMDbData: "IMDB_TEMPLATE_TEXT"
+IMDbData: "IMDB_TEMPLATE_TXT"
 
 Title: "<a href={url}>{title}</a>"
 Genres: "{genres}"
